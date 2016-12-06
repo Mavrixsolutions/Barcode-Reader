6d5a80f6c2b49fbc891d15b89bfbc4f480541bbc
Cyber Desigz <ninesol@Ninesols-MacBook-Pro.local>
2016-12-06 10:36:47 +0500

    Initial Commit

---
+++ barcodereader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  barcodereader
+//
+//  Created by Cyber Desigz on 12/6/16.
+//  Copyright © 2016 Cyber Desigz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ barcodereader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  barcodereader
+//
+//  Created by Cyber Desigz on 12/6/16.
+//  Copyright © 2016 Cyber Desigz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
